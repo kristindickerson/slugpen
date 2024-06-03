4430a9f5725e7fc5eacbbd6fa929f90af698479e
Kristin Dickerson <kdickerson97@gmail.com>
2024-06-03 07:45:36 -0700

    delete old

--- README.md
+++
@@ -1,2 +0,0 @@
-# slugpen
-working repo for a parsing software for data collected by a heat-flow measurement system
--- myRestoreButtonCallbackFcn.m
+++
@@ -1,7 +0,0 @@
-function myRestoreButtonCallbackFcn(hobj, event, originalCallback,H)
-% Responds to pressing the restore button in the axes' toolbar. 
-% originalCallback is a function handle to the original callback 
-%   function for this button. 
-    originalCallback(hobj,event) % Evaluate original callback
-    
-    H.Axes.Raw.XLim = H.Axis_Limits.AxLims_Raw;
